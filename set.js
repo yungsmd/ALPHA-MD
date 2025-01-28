14e60605939583e94f3efd675f89ed3ce1afe83b
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-01-28 06:46:00 +0300

    Add files via upload

---
+++ set.js
@@ -0,0 +1,59 @@
+const fs = require('fs-extra');
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('set.env'))
+    require('dotenv').config({ path: __dirname + '/set.env' });
+const path = require("path");
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
+module.exports = { session: process.env.SESSION_ID || 'Scenemd',
+    PREFIXE: process.env.PREFIX || "",
+    GITHUB : process.env.GITHUB|| 'https://github.com/Keithkeizzah/ALPHA-MD1',
+    OWNER_NAME : process.env.OWNER_NAME || "Keith",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "254748387615",  
+    GCF : process.env.GROUP_CONTROL || "yes",
+    ANTI_VV : process.env.ANTI_VV || "yes",               
+    AUTO_REPLY : process.env.AUTO_REPLY || "yes",              
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || 'viewed by alpha md',
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || 'yes',
+    AUTO_REACT: process.env.AUTO_REACTION || "non",  
+    AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || 'non',              
+    CHATBOT: process.env.CHAT_BOT || "off",              
+    AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "yes",
+    AUTO_BLOCK: process.env.BLOCK_ALL || 'no',
+    AUTOBIO: process.env.AUTOBIO || 'yes',              
+    GURL: process.env.GURL  || "https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47",
+    WEBSITE :process.env.GURL || "https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47",
+    CAPTION : process.env.CAPTION || "ALPHA-MD",
+    BOT : process.env.BOT_NAME || 'ALPHA_MD',
+    URL : process.env.BOT_MENU_LINKS || '',
+    MODE: process.env.PUBLIC_MODE || "no",              
+    TIMEZONE: process.env.TIMEZONE || "Africa/Nairobi", 
+    PM_PERMIT: process.env.PM_PERMIT || 'no',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME || null,
+    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY || null,
+    WARN_COUNT : process.env.WARN_COUNT || '3' ,
+    ANTICALL_MSG : process.env.ANTICALL_MESSAGE || 'THIS IS ALPHA MD.I DECLINE THIS CALL BECAUSE MY OWNER IS BUSY' ,              
+    ETAT : process.env.PRESENCE || '',
+    GEMINI_API_KEY : process.env.GEMINI_API_KEY || 'AIzaSyCcZqDMBa8FcAdBxqE1o6YYvzlygmpBx14',
+    DP : process.env.STARTING_BOT_MESSAGE || "yes",
+    ANTIDELETE : process.env.ANTIDELETE || "yes",               
+    ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
+    ANTICALL: process.env.ANTICALL || 'yes',              
+    CHAT_BOT : process.env.CHAT_BOT || 'no',  
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgresql://flashmd_user:JlUe2Vs0UuBGh0sXz7rxONTeXSOra9XP@dpg-cqbd04tumphs73d2706g-a/flashmd"
+        : "postgresql://flashmd_user:JlUe2Vs0UuBGh0sXz7rxONTeXSOra9XP@dpg-cqbd04tumphs73d2706g-a/flashmd",
+    W_M: null, // Add this line
+};
+let fichier = require.resolve(__filename);
+fs.watchFile(fichier, () => {
+    fs.unwatchFile(fichier);
+    console.log(`mise à jour ${__filename}`);
+    delete require.cache[fichier];
+    require(fichier);
+});
