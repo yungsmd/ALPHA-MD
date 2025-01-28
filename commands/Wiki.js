2d1c8cfb94590845c98abd04437854a4c5aa4f43
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-01-28 07:08:44 +0300

    Create Wiki.js

---
+++ commands/Wiki.js
@@ -0,0 +1,41 @@
+
+const { keith } = require("../keizzah/keith");
+const wiki = require('wikipedia');
+
+keith({
+  nomCom: "wiki",
+  aliases: ["wikipedia", "wikipeda"],
+  reaction: '⚔️',
+  categorie: "search"
+}, async (zk, dest, context) => {
+  const { repondre, arg, ms } = context;
+
+  // Ensure that the search term is provided
+  const text = arg.join(" ").trim(); 
+
+  try {
+    if (!text) return repondre(`Provide the term to search,\nE.g What is JavaScript!`);
+    
+    // Fetch summary from Wikipedia
+    const con = await wiki.summary(text);
+    
+    // Format the reply message
+    const texa = `
+*📚 Wikipedia Summary 📚*
+
+🔍 *Title*: _${con.title}_
+
+📝 *Description*: _${con.description}_
+
+💬 *Summary*: _${con.extract}_
+
+🔗 *URL*: ${con.content_urls.mobile.page}
+
+> Powered by Alpha Md
+    `;
+    repondre(texa);
+  } catch (err) {
+    console.error(err);
+    repondre(`Got 404. I did not find anything!`);
+  }
+});
