6870ab69914bf138bfa27f0de1e95fef8c5353aa
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-01-28 15:26:24 +0300

    Update Axiosget.js

--- commands/Axiosget.js
+++ commands/Axiosget.js
@@ -35,4 +35,58 @@ keith({
     await repondre("Sorry, there was an error retrieving the news. Please try again later.\n" + error);
   }
 });
+keith({
+  nomCom: "bible",
+  reaction: '🎎',
+  categorie: "General"
+}, async (dest, zk, commandeOptions) => {
+  const { repondre, arg, ms } = commandeOptions;
+  const reference = arg.join(" ");
+  
+  if (!reference) {
+    return repondre("Please specify the book, chapter, and verse you want to read. Example: bible john 3:16");
+  }
+  
+  try {
+    const response = await axios.get(`https://bible-api.com/${reference}`);
+    
+    if (!response.data) {
+      return repondre("Invalid reference. Example: bible john 3:16");
+    }
+    
+    const data = response.data;
+    const messageText = `
+ᬑ *ALPHA HOLY BIBLE* ᬒ
+
+⧭ *_WE'RE READING:_* ${data.reference}
+
+⧭ *_NUMBER OF VERSES:_* ${data.verses.length}
+
+⧭ *_NOW READ:_* ${data.text}
+
+⧭ *_LANGUAGE:_* ${data.translation_name}
+╭────────────────◆
+│ *_Powered by keithkeizzah._*
+╰─────────────────◆ `;
+    
+    await zk.sendMessage(dest, {
+      text: messageText,
+      contextInfo: {
+        externalAdReply: {
+          title: "ALPHA-MD HOLY BIBLE",
+          body: `We're reading: ${data.reference}`,
+          mediaType: 1,
+          thumbnailUrl: "https://files.catbox.moe/zt9ie6.jpg", 
+          sourceUrl: conf.GURL,
+          showAdAttribution: true, 
+        },
+      },
+    }, { quoted: ms });
+    
+  } catch (error) {
+    console.error("Error fetching Bible passage:", error);
+    await repondre("An error occurred while fetching the Bible passage. Please try again later.");
+  }
+});
+
  
