b98ecfcd2f7164df0d1573481138ecc1d64b5d57
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-01-28 20:07:44 +0300

    Update Image_dl_search.js

--- commands/Image_dl_search.js
+++ commands/Image_dl_search.js
@@ -1,11 +1,10 @@
-
 const { keith } = require('../keizzah/keith');
-var gis = require('g-i-s');
+const gis = require('g-i-s');
 const conf = require(__dirname + '/../set');
 
 keith({
   nomCom: "img",
-  aliases: ["image", images"],
+  aliases: ["image", "images"],
   categorie: "Search",
   reaction: "📷"
 }, async (dest, zk, commandeOptions) => {
@@ -17,31 +16,35 @@ keith({
   }
 
   const searchTerm = arg.join(" ");
-  gis(searchTerm, sendImage);
+  gis(searchTerm, (error, results) => sendImage(error, results));
 
   function sendImage(error, results) {
     if (error) {
       repondre("Oops, an error occurred.");
-    } else {
-      for (var i = 0; i < 5; i++) {
-        zk.sendMessage(dest, {
-          image: { url: results[i].url },
-          caption: `Downloaded by ${conf.BOT}`
-        },
-        {
-          contextInfo: {
-            externalAdReply: {
-              title: "Image Search Result",
-              body: `Here's the image you searched for: ${searchTerm}`,
-              thumbnailUrl: results[i].url,
-              sourceUrl: conf.GURL,
-              mediaType: 1,
-              showAdAttribution: true
-            }
+      return;
+    }
+
+    if (!results || results.length === 0) {
+      repondre("No images found.");
+      return;
+    }
+
+    for (let i = 0; i < Math.min(results.length, 5); i++) {
+      zk.sendMessage(dest, {
+        image: { url: results[i].url },
+        caption: `Downloaded by ${conf.BOT}`
+      }, {
+        contextInfo: {
+          externalAdReply: {
+            title: "Image Search Result",
+            body: `Here's the image you searched for: ${searchTerm}`,
+            thumbnailUrl: results[i].url,
+            sourceUrl: conf.GURL,
+            mediaType: 1,
+            showAdAttribution: true
           }
-        },
-        { quoted: ms });
-      }
+        }
+      }, { quoted: ms });
     }
   }
 });
