d521beee7f5b1b6bdb18913f834b69a30d88c68d
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-01-29 02:45:58 +0300

    Update Url.js

--- commands/Url.js
+++ commands/Url.js
@@ -2,17 +2,19 @@ const { keith } = require("../keizzah/keith");
 const { Catbox } = require("node-catbox");
 const fs = require('fs-extra');
 const { downloadAndSaveMediaMessage } = require('@whiskeysockets/baileys');
-const conf = require(__dirname + '/../set');
 
 // Initialize Catbox
 const catbox = new Catbox();
 
 // Function to upload a file to Catbox and return the URL
 async function uploadToCatbox(filePath) {
+  if (!fs.existsSync(filePath)) {
+    throw new Error("File does not exist");
+  }
   try {
     const uploadResult = await catbox.uploadFile({ path: filePath });
-    if (uploadResult && uploadResult.fileUrl) {
-      return uploadResult.fileUrl;  // Assuming uploadResult has a fileUrl property
+    if (uploadResult) {
+      return uploadResult;
     } else {
       throw new Error("Error retrieving file link");
     }
@@ -21,37 +23,44 @@ async function uploadToCatbox(filePath) {
   }
 }
 
-// Command to upload image, video, audio, or other media file
+// Command to upload image, video, or audio file
 keith({
-  nomCom: 'url',          // Command to trigger the function
-  categorie: "General",   // Command category
-  reaction: '👨🏿‍💻'       // Reaction to use on command
+  'nomCom': 'url',       // Command to trigger the function
+  'categorie': "General", // Command category
+  'reaction': '👨🏿‍💻'    // Reaction to use on command
 }, async (groupId, client, context) => {
-  const { msgRepondu, repondre, ms } = context;
+  const { msgRepondu, repondre } = context;
 
   // If no message (image/video/audio) is mentioned, prompt user
   if (!msgRepondu) {
-    return repondre("Please mention an image, video, audio document, sticker, or any other media.");
+    return repondre("Please mention an image, video, or audio.");
   }
 
   let mediaPath;
 
-  // Check and download the media from the message
+  // Check if the message contains a video
   if (msgRepondu.videoMessage) {
-    mediaPath = await downloadAndSaveMediaMessage(msgRepondu, 'video');
-  } else if (msgRepondu.stickerMessage) {
-    mediaPath = await downloadAndSaveMediaMessage(msgRepondu, 'sticker');
-  } else if (msgRepondu.gifMessage) {
-    mediaPath = await downloadAndSaveMediaMessage(msgRepondu, 'gif');
-  } else if (msgRepondu.documentMessage) {
-    mediaPath = await downloadAndSaveMediaMessage(msgRepondu, 'document');
-  } else if (msgRepondu.imageMessage) {
-    mediaPath = await downloadAndSaveMediaMessage(msgRepondu, 'image');
-  } else if (msgRepondu.audioMessage) {
-    mediaPath = await downloadAndSaveMediaMessage(msgRepondu, 'audio');
+    mediaPath = await client.downloadAndSaveMediaMessage(msgRepondu.videoMessage);
+  }
+ else if (msgRepondu.gifMessage) {
+    mediaPath = await client.downloadAndSaveMediaMessage(msgRepondu.gifMessage);
+  }
+ else if (msgRepondu.stickerMessage) {
+    mediaPath = await client.downloadAndSaveMediaMessage(msgRepondu.stickerMessage);
+  }
+else if (msgRepondu.documentMessage) {
+    mediaPath = await client.downloadAndSaveMediaMessage(msgRepondu.documentMessage);
+  }
+  // Check if the message contains an image
+  else if (msgRepondu.imageMessage) {
+    mediaPath = await client.downloadAndSaveMediaMessage(msgRepondu.imageMessage);
+  }
+  // Check if the message contains an audio file
+  else if (msgRepondu.audioMessage) {
+    mediaPath = await client.downloadAndSaveMediaMessage(msgRepondu.audioMessage);
   } else {
-    // If no supported media is found, prompt user
-    return repondre("Please mention an image, video, audio, document, sticker, or any other media.");
+    // If no media (image, video, or audio) is found, prompt user
+    return repondre("Please mention an image, video, or audio.");
   }
 
   try {
@@ -61,21 +70,8 @@ keith({
     // Delete the local media file after upload
     fs.unlinkSync(mediaPath);
 
-    // Send the file URL to the user
-    await client.sendMessage(groupId, {
-      text: fileUrl,
-      contextInfo: {
-        externalAdReply: {
-          title: `${conf.BOT} URL GENERATOR`,
-          body: "Here's your awesome 😎 link!",
-          thumbnailUrl: conf.URL,
-          sourceUrl: conf.GURL,
-          mediaType: 1,
-          showAdAttribution: true
-        }
-      }
-    }, { quoted: ms });
-
+    // Respond with the URL of the uploaded file
+    repondre(fileUrl);
   } catch (error) {
     console.error("Error while creating your URL:", error);
     repondre("Oops, there was an error.");
