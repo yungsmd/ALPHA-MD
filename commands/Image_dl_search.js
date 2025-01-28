1d9a9ffcfd5d4f7efbd9bc5130a749b4351f39fe
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-01-28 20:59:29 +0300

    Update Image_dl_search.js

--- commands/Image_dl_search.js
+++ commands/Image_dl_search.js
@@ -1,5 +1,6 @@
 const { keith } = require('../keizzah/keith');
 const gis = require('g-i-s');
+const axios = require('axios');
 const conf = require(__dirname + '/../set');
 
 keith({
@@ -47,3 +48,42 @@ keith({
     }
   }
 });
+
+keith({
+  nomCom: 'messi',
+  categorie: 'image',
+  reaction: '😋'
+}, async (dest, zk, context) => {
+  const { repondre: sendMessage, ms } = context;
+  try {
+    const response = await axios.get("https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/Messi.json");
+    const images = response.data;
+
+    if (!Array.isArray(images) || images.length === 0) {
+      throw new Error("No images found in the response.");
+    }
+
+    for (let i = 0; i < 5; i++) {
+      const randomImage = Math.floor(Math.random() * images.length);
+      const image = images[randomImage];
+      await zk.sendMessage(dest, {
+        image: { url: image },
+        caption: `*Downloaded by ${conf.BOT}*`,
+        contextInfo: {
+          externalAdReply: {
+            title: "Modern-Logo Search Result",
+            body: `Here's an inspiring logo related to: messi`,
+            thumbnailUrl: image,
+            sourceUrl: conf.GURL,
+            mediaType: 1,
+            showAdAttribution: true
+          }
+        }
+      }, { quoted: ms });
+    }
+  } catch (error) {
+    console.error("Error occurred while retrieving data:", error);
+    sendMessage("Error occurred while retrieving data: " + error.message);
+  }
+});
+
