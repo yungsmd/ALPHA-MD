063812af351b12fb17b211026ee322c81a9be554
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-01-31 18:42:47 +0300

    Update Groupe.js

--- commands/Groupe.js
+++ commands/Groupe.js
@@ -47,3 +47,110 @@ keith({ nomCom: "tagall", categorie: 'Group', reaction: "📣" }, async (dest, z
     repondre('command reserved for admins');
   }
 });
+
+keith({ nomCom: "invite", categorie: 'Group', reaction: "🙋" }, async (dest, zk, commandeOptions) => {
+  const { repondre, nomGroupe, nomAuteurMessage, verifGroupe } = commandeOptions;
+  if (!verifGroupe) {
+    return repondre("Wait bro, do you want the link to my DM?");
+  }
+
+  const link = await zk.groupInviteCode(dest);
+  const lien = `https://chat.whatsapp.com/${link}`;
+  const mess = `Hello ${nomAuteurMessage}, here is the group link of ${nomGroupe} \n\nClick Here To Join: ${lien}`;
+  repondre(mess);
+});
+
+/** Promote a member to admin */
+keith({ nomCom: "promote", categorie: 'Group', reaction: "👨🏿‍💼" }, async (dest, zk, commandeOptions) => {
+  let { repondre, msgRepondu, infosGroupe, auteurMsgRepondu, verifGroupe, auteurMessage, superUser, idBot } = commandeOptions;
+  let membresGroupe = verifGroupe ? await infosGroupe.participants : [];
+
+  if (!verifGroupe) return repondre("For groups only");
+
+  const verifMember = (user) => membresGroupe.some(m => m.id === user);
+
+  const memberAdmin = (membresGroupe) => membresGroupe.filter(m => m.admin != null).map(m => m.id);
+
+  const admins = verifGroupe ? memberAdmin(membresGroupe) : [];
+
+  const admin = verifGroupe ? admins.includes(auteurMsgRepondu) : false;
+  const membre = verifMember(auteurMsgRepondu);
+  const autAdmin = verifGroupe ? admins.includes(auteurMessage) : false;
+  const zkad = verifGroupe ? admins.includes(idBot) : false;
+
+  try {
+    if (autAdmin || superUser) {
+      if (msgRepondu) {
+        if (zkad) {
+          if (membre) {
+            if (!admin) {
+              const txt = `🎊🍾 @${auteurMsgRepondu.split("@")[0]} has been promoted as a group Admin.`;
+              await zk.groupParticipantsUpdate(dest, [auteurMsgRepondu], "promote");
+              zk.sendMessage(dest, { text: txt, mentions: [auteurMsgRepondu] });
+            } else {
+              return repondre("This member is already an administrator of the group.");
+            }
+          } else {
+            return repondre("This user is not part of the group.");
+          }
+        } else {
+          return repondre("Sorry, I cannot perform this action because I am not an administrator of the group.");
+        }
+      } else {
+        repondre("Please tag the member to be nominated.");
+      }
+    } else {
+      return repondre("Sorry, I cannot perform this action because you are not an administrator of the group.");
+    }
+  } catch (e) {
+    repondre("Oops, something went wrong: " + e);
+  }
+});
+
+/** Demote a member */
+keith({ nomCom: "demote", categorie: 'Group', reaction: "👨🏿‍💼" }, async (dest, zk, commandeOptions) => {
+  let { repondre, msgRepondu, infosGroupe, auteurMsgRepondu, verifGroupe, auteurMessage, superUser, idBot } = commandeOptions;
+  let membresGroupe = verifGroupe ? await infosGroupe.participants : [];
+
+  if (!verifGroupe) return repondre("For groups only");
+
+  const verifMember = (user) => membresGroupe.some(m => m.id === user);
+
+  const memberAdmin = (membresGroupe) => membresGroupe.filter(m => m.admin != null).map(m => m.id);
+
+  const admins = verifGroupe ? memberAdmin(membresGroupe) : [];
+
+  const admin = verifGroupe ? admins.includes(auteurMsgRepondu) : false;
+  const membre = verifMember(auteurMsgRepondu);
+  const autAdmin = verifGroupe ? admins.includes(auteurMessage) : false;
+  const zkad = verifGroupe ? admins.includes(idBot) : false;
+
+  try {
+    if (autAdmin || superUser) {
+      if (msgRepondu) {
+        if (zkad) {
+          if (membre) {
+            if (!admin) {
+              repondre("This member is not a group administrator.");
+            } else {
+              const txt = `@${auteurMsgRepondu.split("@")[0]} has been removed from their position as a group administrator.`;
+              await zk.groupParticipantsUpdate(dest, [auteurMsgRepondu], "demote");
+              zk.sendMessage(dest, { text: txt, mentions: [auteurMsgRepondu] });
+            }
+          } else {
+            return repondre("This user is not part of the group.");
+          }
+        } else {
+          return repondre("Sorry, I cannot perform this action because I am not an administrator of the group.");
+        }
+      } else {
+        repondre("Please tag the member to be removed.");
+      }
+    } else {
+      return repondre("Sorry, I cannot perform this action because you are not an administrator of the group.");
+    }
+  } catch (e) {
+    repondre("Oops, something went wrong: " + e);
+  }
+});
+
