392409a57e28ceb7badebfde197337bb4e770235
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-02-05 11:55:18 +0300

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -5,6 +5,45 @@ const { writeFile } = require("fs/promises");
 const fs = require('fs-extra');
 const moment = require("moment-timezone");
 
+
+keith({
+  nomCom: 'report',
+  aliases: 'spread',
+  desc: 'report anything to the bot developer',
+  categorie: "owner",
+  reaction: '⚪'
+}, async (bot, zk, context) => {
+  const { arg, repondre, superUser, nomAuteurMessage } = context;
+
+  if (!arg[0]) {
+    return repondre("After the command *broadcast*, type your message to be sent to the specified contacts.");
+  }
+
+  if (!superUser) {
+    return repondre("Only for the owner.");
+  }
+
+  // Specified contacts
+  const contacts = [
+    '254748387615@s.whatsapp.net',
+    '254110190196@s.whatsapp.net',
+    '254796299159@s.whatsapp.net'
+  ];
+
+  await repondre("*ALPHA-MD is sending your message to Developer contacts 💀*...");
+
+  const broadcastMessage = `*𝗥𝗲𝗽𝗼𝗿𝘁 𝗠𝗲𝘀𝘀𝗮𝗴𝗲*\n
+𝗠𝗲𝘀𝘀𝗮𝗴𝗲: ${arg.join(" ")}\n
+𝗦𝗲𝗻𝗱𝗲𝗿 𝗡𝗮𝗺𝗲 : ${nomAuteurMessage}`;
+
+  for (let contact of contacts) {
+    await zk.sendMessage(contact, {
+      image: { url: 'https://i.imgur.com/HDLN3If.jpeg' },
+      caption: broadcastMessage
+    });
+  }
+});
+
 keith({
   nomCom: 'broadcast',
   aliase: 'spread',
