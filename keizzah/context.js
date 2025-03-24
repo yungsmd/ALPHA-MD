e73a0a56a5ad4b3b5252af6eb6dbba51a3654858
keith <157195350+Keithkeizzah@users.noreply.github.com>
2025-03-24 10:07:57 +0300

    @keithkeizzah

---
+++ keizzah/context.js
@@ -0,0 +1,41 @@
+module.exports = {
+  
+  getContextInfo: (ms) => {
+    return {
+      mentionedJid: [ms.sender || ms.from], 
+      forwardingScore: 999,
+      isForwarded: true, 
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363266249040649@newsletter', 
+        newsletterName: 'Keith Support', 
+        serverMessageId: 143 
+      }
+    };
+  },
+
+
+  repondre: async (zk, dest, ms, text, options = {}) => {
+    const contextInfo = {
+      ...module.exports.getContextInfo(ms), 
+      ...options.contextInfo 
+    };
+
+    await zk.sendMessage(dest, {
+      text: text,
+      contextInfo: contextInfo
+    }, { quoted: ms }); 
+  },
+
+
+  sendMessage: async (zk, dest, ms, options) => {
+    const contextInfo = {
+      ...module.exports.getContextInfo(ms), 
+      ...options.contextInfo 
+    };
+
+    await zk.sendMessage(dest, {
+      ...options,
+      contextInfo: contextInfo
+    }, { quoted: ms }); 
+  }
+};
